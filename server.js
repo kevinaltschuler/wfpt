4023f61e10e53cc6ae8917a3f627f5b64339b48a
kaltschu <kevinaltschuler@gmail.com>
2017-02-12 15:29:32 -0500

    done with config

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const config = require('./webpack.config');
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
-  historyApiFallback: true
+  historyApiFallback: true,
+  stats: { colors: true },
 }).listen(5000, 'localhost', (err) => {
   if (err) {
     console.log(err);
