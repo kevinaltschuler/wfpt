e4727c35aeba7b27e9160ff1c653eb64bb783b1d
kaltschu <kevinaltschuler@gmail.com>
2017-02-14 21:07:44 -0500

    persistent container

--- js/App.js
+++ js/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { Router, Route, browserHistory } from 'react-router';
+import { Router, Route, browserHistory, IndexRedirect } from 'react-router';
+import HomePage from './HomePage/HomePage';
 import AppContainer from './AppContainer';
 
 export default () => (
-  <div>
-    <Router history={browserHistory}>
-      <Route path="/" component={AppContainer}></Route>
-    </Router>
-  </div>
+  <Router history={browserHistory}>
+    <Route path="/" component={AppContainer}>
+      <IndexRedirect to="/home" />
+      <Route path="/home" component={HomePage} />
+      <Route path="/home" component={HomePage} />
+      <Route path="/home" component={HomePage} />
+    </Route>
+  </Router>
 );
--- js/AppContainer.js
+++ js/AppContainer.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import Header from './Header/Header';
 
-const AppContainer = () => (
-  <Header />
+const AppContainer = (props) => (
+  <div>
+    <Header />
+    { props.children }
+  </div>
 );
+
+AppContainer.propTypes = {
+  children: React.PropTypes.object,
+};
 
 export default AppContainer;
