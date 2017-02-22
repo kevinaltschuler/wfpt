b3a3c031fc029c544bfad965ad0de0d75e754bcc
MaxRais <max.j.rais@gmail.com>
2017-02-21 21:42:55 -0500

    some stuff

--- models/models.js
+++ models/models.js
@@ -6,3 +6,12 @@ connection.once('open', function() {
     console.log('connected to database');
 });
 
+var tourModel = require('./tourSchema');
+var trailerModel = require('./trailerSchema');
+
+var models = {
+    tour: tourModel,
+    trailerModel: trailerModel
+};
+
+module.exports = models;
