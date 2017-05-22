9a7c6d1c2b9faf2adaff1cae4ab44e93b848a5a1
MaxRais <max.j.rais@gmail.com>
2017-05-22 16:12:26 -0400

    added auth to admin routes and changed url scheme a little

--- app.js
+++ app.js
@@ -177,7 +177,7 @@ app.get('/failure', (req, res) => {
     });
 });
 
-app.post('/addTour', (req, res) => {
+app.post('/tour/create', passport.authenticate('basic', { session: false }), (req, res) => {
     var Tour = models.tour;
 
     var t = new Tour({
@@ -200,7 +200,7 @@ app.post('/addTour', (req, res) => {
     });
 });
 
-app.post('/addTrailer', (req, res) => {
+app.post('/trailer/create', passport.authenticate('basic', { session: false }), (req, res) => {
     var Trailer = models.trailer;
 
     var t = new Trailer({
@@ -224,7 +224,7 @@ app.post('/addTrailer', (req, res) => {
     });
 });
 
-app.post('/addPress', (req, res) => {
+app.post('/press/create', passport.authenticate('basic', { session: false }), (req, res) => {
     var Press = models.press;
 
     var p = new Press({
@@ -247,7 +247,7 @@ app.post('/addPress', (req, res) => {
     });
 });
 
-app.post('/poll/update/:id', (req, res) => {
+app.post('/poll/:id/update', passport.authenticate('basic', { session: false }), (req, res) => {
     const Poll = models.poll;
     Poll.findOneAndUpdate({_id: req.params.id}, {$set: { question: req.body.question, yes: 0, no: 0}}, function(err) {
         if(err){
@@ -260,7 +260,7 @@ app.post('/poll/update/:id', (req, res) => {
     });
 });
 
-app.post('/delete/tour/:id', (req, res) => {
+app.post('/tour/:id/delete', passport.authenticate('basic', { session: false }), (req, res) => {
     var Tour = models.tour;
     Tour.findByIdAndRemove(req.params.id, function (err) {
         if (err) {
@@ -274,7 +274,7 @@ app.post('/delete/tour/:id', (req, res) => {
     });
 });
 
-app.post('/delete/trailer/:id', (req, res) => {
+app.post('/trailer/:id/delete', passport.authenticate('basic', { session: false }), (req, res) => {
     var Trailer = models.trailer;
     Trailer.findByIdAndRemove(req.params.id, function (err) {
         if (err) {
@@ -288,7 +288,7 @@ app.post('/delete/trailer/:id', (req, res) => {
     });
 });
 
-app.post('/delete/press/:id', (req, res) => {
+app.post('/press/:id/delete', passport.authenticate('basic', { session: false }), (req, res) => {
     var Press = models.press;
     Press.findByIdAndRemove(req.params.id, function (err) {
         if (err) {
@@ -421,8 +421,7 @@ app.use('/sendRequest', (req, res) => {
     });
 });
 
-
-app.use('/vote/:id', (req, res) => {
+app.use('/trailer/:id/vote', (req, res) => {
     const Trailer = models.trailer;
     Trailer.findOneAndUpdate({_id: req.params.id}, {$inc: { votes: 1 }}, function(err) {
         if(err){
@@ -435,7 +434,7 @@ app.use('/vote/:id', (req, res) => {
     });
 });
 
-app.use('/poll/vote/:id/yes', (req, res) => {
+app.use('/poll/:id/vote/yes', (req, res) => {
     const Poll = models.poll;
     Poll.findOneAndUpdate({_id: req.params.id}, {$inc: { yes: 1 }}, function(err) {
         if(err){
@@ -448,7 +447,7 @@ app.use('/poll/vote/:id/yes', (req, res) => {
     });
 });
 
-app.use('/poll/vote/:id/no', (req, res) => {
+app.use('/poll/:id/vote/no', (req, res) => {
     const Poll = models.poll;
     Poll.findOneAndUpdate({_id: req.params.id}, {$inc: { no: 1 }}, function(err) {
         if(err){
